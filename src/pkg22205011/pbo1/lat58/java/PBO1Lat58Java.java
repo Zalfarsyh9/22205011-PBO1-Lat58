8fe569b2805e6c21925ca1213f96cc3e66b1ae6d
Zalfarsyh9 <145966652+Zalfarsyh9@users.noreply.github.com>
2023-11-29 13:27:26 +0700

    Add files via upload

---
+++ src/pkg22205011/pbo1/lat58/java/PBO1Lat58Java.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg22205011.pbo1.lat58.java;
+
+/**
+ *
+ * @author User Nama : Zalfa Rosiyah Riski Nim : 22205011 Jurusan : Teknik
+ * Informatika Deskripsi : Membuat program dengan tambah kurang bilangan
+ */
+class Bilangan {
+
+    protected int x;
+    protected int y;
+
+    public Bilangan() {
+        this.x = 3;
+        this.y = 4;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+class JumlahBilangan extends Bilangan {
+
+    public void tampilHasilJumlah() {
+        System.out.println("Hasil Penjumlahan = " + (getX() + getY()));
+    }
+}
+
+class SelisihBilangan extends Bilangan {
+
+    public void tampilSelisih() {
+        System.out.println("Hasil Selisih " + getX() + " - " + getY() + " = " + (getX() - getY()));
+    }
+}
+
+public class PBO1Lat58Java {
+
+    public static void main(String[] args) {
+        //Membuat objek JumlahBilangan
+        JumlahBilangan jm = new JumlahBilangan();
+        jm.tampilHasilJumlah();
+
+        //Membuat objek Selisih Bilangan
+        SelisihBilangan sb = new SelisihBilangan();
+        sb.tampilSelisih();
+
+    }
+}
